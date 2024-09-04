cd2a58395ef4ecac84a1be1a0919095601a8f691
tnegre <tnegre@open-dsi.fr>
2024-09-04 10:15:09 +0200

    Use multicurrency supplier price instead of recomputing from EUR supplier price

--- htdocs/product/stock/replenish.php
+++ htdocs/product/stock/replenish.php
@@ -185,7 +185,7 @@ if ($action == 'order' && GETPOST('valid')) {
 							$productsupplier->getMultiLangs();
 						}
 
-						// if we use supplier description of the products
+						// If we use supplier description of the products
 						if (!empty($productsupplier->desc_supplier) && !empty($conf->global->PRODUIT_FOURN_TEXTS)) {
 							$desc = $productsupplier->desc_supplier;
 						} else {
@@ -196,6 +196,13 @@ if ($action == 'order' && GETPOST('valid')) {
 							// TODO Get desc in language of thirdparty
 						}
 
+						// If we use multicurrency
+						if (isModEnabled('multicurrency') && !empty($productsupplier->fourn_multicurrency_code) && $productsupplier->fourn_multicurrency_code != $conf->currency) {
+							$line->multicurrency_code = $productsupplier->fourn_multicurrency_code;
+							$line->fk_multicurrency = $productsupplier->fourn_multicurrency_id;
+							$line->multicurrency_subprice = $productsupplier->fourn_multicurrency_unitprice;
+						}
+
 						$line->tva_tx = $productsupplier->vatrate_supplier;
 						$line->subprice = $productsupplier->fourn_pu;
 						$line->total_ht = $productsupplier->fourn_pu * $qty;
@@ -203,8 +210,8 @@ if ($action == 'order' && GETPOST('valid')) {
 						$line->total_tva = $line->total_ht * $tva;
 						$line->total_ttc = $line->total_ht + $line->total_tva;
 						$line->remise_percent = $productsupplier->remise_percent;
-						$line->ref_fourn = $productsupplier->ref_supplier;
-						$line->type = $productsupplier->type;
+						$line->ref_supplier = $productsupplier->ref_supplier;
+						$line->product_type = $productsupplier->type;
 						$line->fk_unit = $productsupplier->fk_unit;
 						$suppliers[$productsupplier->fourn_socid]['lines'][] = $line;
 					}
@@ -254,17 +261,18 @@ if ($action == 'order' && GETPOST('valid')) {
 						$line->localtax2_tx,
 						$line->fk_product,
 						0,
-						$line->ref_fourn,
+						$line->ref_supplier,
 						$line->remise_percent,
 						'HT',
 						0,
-						$line->type,
+						$line->product_type,
 						0,
 						false,
 						null,
 						null,
 						0,
-						$line->fk_unit
+						$line->fk_unit,
+						$line->multicurrency_subprice ?? 0
 					);
 				}
 				if ($result < 0) {
